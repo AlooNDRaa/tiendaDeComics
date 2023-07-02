037bcabac1b7d9879a3659e33476d6f8ac6c7179
Alondra <alonngadea@gmail.com>
2023-07-02 14:54:27 -0300

    Subo los cambios en tienda, arreglos generales que vi pertinente

--- Tienda.js
+++ Tienda.js
@@ -15,3 +15,22 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// icono desplazable 
+
+const iconoCompraContainer = document.getElementById('icono-compra-container');
+const carrito = document.getElementById('panel-carrito');
+let carritoVisible = false;
+
+iconoCompraContainer.addEventListener('click', () => {
+  if (!carritoVisible) {
+    carrito.style.top = '100px'; // Ajusta la posición para mostrar el carrito debajo de la imagen
+    carrito.style.height = '150px'; // Ajusta la altura del carrito
+    carritoVisible = true;
+  } else {
+    carrito.style.top = '-200px'; // Oculta el carrito moviéndolo nuevamente hacia arriba
+    carrito.style.height = '0';
+    carritoVisible = false;
+  }
+});
+
