0ed4aa32aa07ce84f6f10e9bedc2aa65b99f62e8
ValentinaBS <vbsanchez16@gmail.com>
2023-07-18 17:14:16 -0300

    refactor: dynamic comics and minor improvements

--- assets/js/comics.js
+++ assets/js/comics.js
@@ -1,3 +1,43 @@
+// cards dinámicas
+const cardContainer = document.querySelector(".contenedor-tarjeta");
+
+function createCard(comic) {
+  return `
+    <article class="tarjeta">
+      <img 
+        class="comic-img"
+        src="../images/comics/${comic.img}" 
+        alt="${comic.name}" 
+      />
+      <div class="comic-descripcion">
+        <h2 class="comic-titulo">
+          ${comic.name}
+        </h2>
+        <div class="comic-compra">
+          <p class="comic-precio">
+            $${comic.price}
+          </p>
+          <button class="comic-boton">
+            Añadir al carrito
+          </button>
+        </div> 
+      </div>
+    </article>
+  `
+}
+
+function printCards(comics) {
+  let template = "";
+
+  for(let comic of comics) {
+    template += createCard(comic)
+  }
+
+  cardContainer.innerHTML += template
+}
+
+printCards(products.comics)
+
 // flecha para arriba 
 let mybutton = document.getElementById("myBtn");
 
@@ -10,3 +50,31 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// icono desplazable 
+const iconoCompraContainer = document.getElementById('icono-compra-container');
+const carrito = document.getElementById('panel-carrito');
+let carritoVisible = false;
+
+iconoCompraContainer.addEventListener('click', (event) => {
+  event.stopPropagation(); // Evita que el evento se propague al documento y cierre el panel
+  if (!carritoVisible) {
+    carrito.style.right = '0';
+    carritoVisible = true;
+  } else {
+    carrito.style.right = '-300px';
+    carritoVisible = false;
+  }
+});
+
+document.addEventListener('click', (event) => {
+  if (carritoVisible && !event.target.closest('#panel-carrito')) {
+    carrito.style.right = '-500px';
+    carritoVisible = false;
+  }
+});
