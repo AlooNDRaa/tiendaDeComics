4fb17096344b4e7f6828a64ee35c0a21e7a9926f
Gadea Alondra <alonngadea@gmail.com>
2023-07-19 09:24:52 -0300

    Fix: display-carrito

--- assets/js/Tienda.js
+++ assets/js/Tienda.js
@@ -16,6 +16,7 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+
 // icono desplazable 
 
 const iconoCompraContainer = document.getElementById('icono-compra-container');
@@ -41,7 +42,7 @@ document.addEventListener('click', (event) => {
 });
 
 
-//// Añadir a carrito
+//// Añadir a carrito por que si no funciona lloro
 
 // Obtener referencia al botón "Añadir al carrito"
 const addToCartButtons = document.querySelectorAll('.btnn button');
@@ -54,16 +55,16 @@ addToCartButtons.forEach(button => {
   button.addEventListener('click', addToCart);
 });
 
-// Objeto para realizar un seguimiento de los productos y su cantidad
+// seguir cantidad de productos o lloro si no sirve
 const cartItems = {};
 let totalValue = 0;
 
 function addToCart(event) {
-  const card = event.target.closest('.card'); // Obtener la tarjeta padre del botón
+  const card = event.target.closest('.card'); // Obtener la tarjeta padre del botón que presiono o lloro
   const productImage = card.querySelector('img').src; // Obtener la URL de la imagen del producto
   const productPrice = card.querySelector('.price').innerText; // Obtener el precio del producto
 
-  // Verificar si el producto ya está en el carrito
+  // verificar y sumar si ya esta o lloro y me arranco los pelos
   const existingProduct = display2Div.querySelector(`.product-container img[src="${productImage}"]`);
   if (existingProduct) {
     // Si el producto ya existe, incrementar el contador en 1
@@ -92,7 +93,7 @@ function addToCart(event) {
     display2Div.appendChild(productContainer);
   }
 
-  // Actualizar el valor total
+  // Actualizar el valor total - analiza el argumento el parse y lo devuelve XD
   totalValue += parseFloat(productPrice.slice(1)); // Convertir el precio a número y sumarlo al totalValue
   valorTotalElement.textContent = `$${totalValue.toFixed(2)}`; // Mostrar el totalValue con dos decimales
 }
