d32437c71f22518acf4e4cdcdaee5b2af710b7ea
unknown <johndoe@example.com>
2023-07-22 00:39:40 -0300

    refactor-rutes

--- assets/js/manga.js
+++ assets/js/manga.js
@@ -15,7 +15,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
